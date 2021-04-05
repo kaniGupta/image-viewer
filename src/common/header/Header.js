e08773657b60621238330aac0a61b8ff881498b1
kaniGupta <kanishkgupta13@gmail.com>
2021-04-05 14:51:03 +0530

    Added a profile page.

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,10 +1,35 @@
-import { Avatar } from '@material-ui/core';
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { withRouter } from 'react-router';
+import { Avatar, Menu, MenuItem } from '@material-ui/core';
 
 import './Header.css';
 
 class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { anchorEl: null };
+  }
+
+  handleClick = (event) => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  handleClose = () => {
+    this.setState({ anchorEl: null });
+  };
+
+  handleProfile = () => {
+    this.handleClose();
+    this.props.history.push('/profile');
+  };
+
+  handleLogout = () => {
+    this.handleClose();
+    sessionStorage.removeItem('access-token');
+    this.props.history.push('/');
+  };
+
   render() {
     return (
       <header className="header">
@@ -24,7 +49,17 @@ class Header extends Component {
               className="header-avatar"
               aria-label="recipe"
               src={this.props.profilePicture}
+              onClick={this.handleClick}
             />
+            <Menu
+              id="simple-menu"
+              anchorEl={this.state.anchorEl}
+              keepMounted
+              open={Boolean(this.state.anchorEl)}
+              onClose={this.handleClose}>
+              <MenuItem onClick={this.handleProfile}>My Account</MenuItem>
+              <MenuItem onClick={this.handleLogout}>Logout</MenuItem>
+            </Menu>
           </div>
         )}
       </header>
@@ -32,4 +67,4 @@ class Header extends Component {
   }
 }
 
-export default Header;
+export default withRouter(Header);
--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Login from './login/Login';
 import Home from './home/Home';
+import Profile from './profile/Profile';
 
 class Controller extends Component {
   render() {
@@ -11,7 +12,8 @@ class Controller extends Component {
         <div>
           <Switch>
             <Route exact path="/" render={() => <Login />} />
-            <Route exact path="/home" render={() => <Home />} />
+            <Route path="/home" render={() => <Home />} />
+            <Route path="/profile" render={() => <Profile />} />
           </Switch>
         </div>
       </BrowserRouter>
